b6516aebae4634a336c0aa096f8cd92dfcadac07
Jani Karvo <jani.karvo@fi.experis.com>
2022-08-23 11:45:03 +0300

    Initial commit

---
+++ src/main/java/com/assignment/newtunes/NewtunesApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.newtunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/newtunes/NewtunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.newtunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewtunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
